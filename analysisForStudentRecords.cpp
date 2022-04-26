69f448fb9e8ff7a7e2f2e6162f31c4cd2f055c25
HusseinHany <hhany3997@gmail.com>
2022-04-27 01:52:58 +0200

    Implement basic struct & constructor

--- analysisForStudentRecords.cpp
+++ analysisForStudentRecords.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Student {
+    int id {};
+    string name {};
+    char sex {};
+    int midTerm_score {}; // from 0 to 20
+    int final_score {};   // from 0 to 80
+    int total_score {};   // 100 max
+
+    Student(int _id, string _name, char _sex, int _midTerm_score, int _final_score){
+        id = _id, name = _name, sex = _sex, midTerm_score = _midTerm_score, final_score = _final_score;
+        total_score = midTerm_score + final_score;
+    }
+};
+
 
 /*
  * struct for 20 student
