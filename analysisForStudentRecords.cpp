13f69997bbf36ad66c15551db0deb944ffee5d6c
HusseinHany <hhany3997@gmail.com>
2022-04-27 01:46:46 +0200

    Fill draft

---
+++ analysisForStudentRecords.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+ * struct for 20 student
+ * id, name, sex, mid-term score, final score, total score
+ * FUNCTIONS
+ * 1. add student
+ * 2. view all students
+ * 3. calc. average of selected student scores
+ * 4. show student who gets the max total score
+ * 5 show studebt who get the min total score
+ * 6. find student by id
+ * 7. sort records by total score
+ *
+ */
+
+int main() {
+
+    return 0;
+}
+
