e57c124f1c887253ab248040ca0adf91d850af67
HusseinHany <hhany3997@gmail.com>
2022-07-18 16:33:19 +0200

    2nd semester gpa

--- FCI_assignments/First Year/gpa_calculator.cpp
+++ FCI_assignments/First Year/gpa_calculator.cpp
@@ -1,38 +1,54 @@
 #include<iostream>
-using namespace std;int main(){
-  int n,creditHours, total_hrs = 0;
-  float gpa, grade, totalGpa_hrs = 0;
-  cout << "Number of subjects:" ;
-  cin >> n;
 
-  for (int i = 1; i <= n; i++) {
-    cout << i << " " << "grade:" ;
-    cin >> grade;
-    if (grade >= 90){
-      gpa = 4;
-    } else if (grade >= 85 && grade < 95) {
-      gpa = 3.6;
-    } else if (grade >= 80 && grade < 85) {
-      gpa = 3.3;
-    } else if (grade >= 75 && grade < 80) {
-      gpa = 3;
-    } else if (grade >= 70 && grade < 75) {
-      gpa = 2.6;
-    } else if (grade >= 65 && grade < 70) {
-      gpa = 2.4;
-    } else if (grade >= 60 && grade < 65) {
-      gpa = 2.2;
-    } else if (grade >= 50 && grade < 60) {
-      gpa = 2;
-    } else if (grade < 50) {
-      gpa = 0;
+using namespace std;
+
+int main() {
+    int n{}, creditHours{}, total_hrs{};
+    float gpa{}, grade{}, totalGpa_hrs{}, last_gpa{};
+    bool avg = false;
+    cout << "Enter last GPA score: ";
+    cin >> last_gpa;
+    cout << "2nd semester number of subjects: ";
+    cin >> n;
+    if (last_gpa)
+        avg = true;
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << " : " << "grade: ";
+        cin >> grade;
+        if (grade >= 95) {
+            gpa = 4;
+        } else if (grade >= 85 && grade < 95) {
+            gpa = 3.6;
+        } else if (grade >= 80 && grade < 85) {
+            gpa = 3.3;
+        } else if (grade >= 75 && grade < 80) {
+            gpa = 3;
+        } else if (grade >= 70 && grade < 75) {
+            gpa = 2.6;
+        } else if (grade >= 65 && grade < 70) {
+            gpa = 2.4;
+        } else if (grade >= 60 && grade < 65) {
+            gpa = 2.2;
+        } else if (grade >= 50 && grade < 60) {
+            gpa = 2;
+        } else if (grade < 50) {
+            gpa = 0;
+        }
+        cout << "Enter number of credit hours for this subject: ";
+        cin >> creditHours;
+        totalGpa_hrs += gpa * creditHours;
+        total_hrs += creditHours;
     }
-    cout << "Enter number of credit hours for this subject: ";
-    cin >> creditHours;
-    totalGpa_hrs += gpa * creditHours;
-    total_hrs += creditHours;
-  }
-  cout << "Your total gpa = " << (float)totalGpa_hrs/total_hrs << endl;
+    float new_gpa = (float) totalGpa_hrs / total_hrs;
+    if (avg)
+        new_gpa = (last_gpa + new_gpa) / 2.0;
+    cout << "Your total gpa = " << new_gpa << endl;
 
-  return 0;
+    return 0;
 }
+/*
+ * N.B
+ * this code only works if 1st semester credit hours equals the 2nd semester credit hours
+ * which is our case here .. i.e gonna support != credits in the future ..
+ */
