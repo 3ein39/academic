8155f7c5139f750280b5eca7373af7d5aa9e5880
HusseinHany <hhany3997@gmail.com>
2022-08-04 16:05:43 +0200

    add push_back()

--- DataStructure/myVector.cpp
+++ DataStructure/myVector.cpp
@@ -47,16 +47,43 @@ public:
             if (val == arr[i]) return i;
         return -1;
     }
+
+    int get_front() {
+        return arr[0];
+    }
+
+    int get_back() {
+        return arr[size - 1];
+    }
+
+    // appending to the end of the array
+    // arr.push_back(val) => void
+    void push_back(int val) {
+        // create new array of bigger size
+        int* new_arr = new int[size + 1];
+        // copy old data
+        for (int i = 0; i < size; i++)
+            new_arr[i] = arr[i];
+        // add new el and increase the size
+        new_arr[size++] = val;
+        // change the pointers
+        swap(new_arr,arr);
+        // remove the useless data
+        delete[] new_arr;
+    }
 };
 
 int main() {
-    myVector v(10);
+    int n = 4;
+    myVector v(n);
 
-    for (int i = 0; i < 10; ++i) {
-        v.set(i, i);
-    }
+    for (int i = 0; i < n; ++i)
+        v.set(i,i);
 
-    v.print();
-    cout << v.find(5) << " " << v.find(55);
+    v.push_back(15);
+    v.push_back(17);
+    v.push_back(19);
+    v.print(); // 0 1 2 3 15 17 19
+    
 }
 
