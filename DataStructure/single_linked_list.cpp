ec6b3bd877c05105dfc3743b183d9f12c77eab9d
HusseinHany <hhany3997@gmail.com>
2022-08-07 15:17:45 +0200

    basic sll practice

--- DataStructure/single_linked_list.cpp
+++ DataStructure/single_linked_list.cpp
@@ -9,6 +9,21 @@ struct Node {
 };
 
 int main() {
+    // creating 4 nodes
+    Node* node1 = new Node(6);
+    Node* node2 = new Node(10);
+    Node* node3 = new Node(8);
+    Node* node4 = new Node(15);
+    // linking them together
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = nullptr;
+
+    cout << node4->data << "\n";
+    cout << node3->next->data << "\n";
+    cout << node2->next->next->data << "\n";
+    cout << node1->next->next->next->data << "\n";
 
     return 0;
 }
