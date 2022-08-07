a8a198eb44faac8f05ecdb9ce2aba08bd6380566
HusseinHany <hhany3997@gmail.com>
2022-08-07 15:03:16 +0200

    implement SLL Node

---
+++ DataStructure/single_linked_list.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next; // pointer to same object
+
+    Node(int data) : data(data) {}
+};
+
+int main() {
+
+    return 0;
+}
