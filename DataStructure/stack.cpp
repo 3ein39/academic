16688a804e36ea33c5056327ac1df163f6063b26
HusseinHany <hhany3997@gmail.com>
2022-08-30 21:27:18 +0200

    write default constructor

--- DataStructure/stack.cpp
+++ DataStructure/stack.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Stack {
+private:
+    int size{};
+    int top{};
+    int* array {};
+public:
+    Stack(int size) : size(size), top(-1) {
+        array = new int[size];
+    }
+    
+};
+
 int main() {
 
     return 0;
