10756e86066c0aad36ad86d0704de906886b9713
HusseinHany <hhany3997@gmail.com>
2022-09-07 14:53:30 +0200

    write oush function

--- DataStructure/sll_stack.cpp
+++ DataStructure/sll_stack.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class Stack {
+    struct Node {
+        int data;
+        Node* next{};
+
+        Node(int data) : data(data) {};
+    };
+    Node* head{};
+
+    void link(Node* first, Node* second) {
+        if (first)
+            first->next = second;
+    }
+public:
+    void push(int data) {
+        // Great match with stack
+        Node* item = new Node(data);
+
+        link(item, head);
+        head = item;
+        // This push function works as long as machine has more ram
+    }
+};
+
 int main() {
     cout << "Hello world";
 
