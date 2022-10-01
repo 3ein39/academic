a93d985872732742f4c83229f444f045210ba880
HusseinHany <hhany3997@gmail.com>
2022-10-01 15:57:55 +0200

    get tree height

--- DataStructure/binary_tree.cpp
+++ DataStructure/binary_tree.cpp
@@ -32,6 +32,15 @@ private:
         return max(max_val, current->data);
     }
 
+    int get_height(Node* current) {
+        int h = 0;
+
+        if (!current)
+            return 0;
+
+        h = max(get_height(current->left), get_height(current->right));
+        return h + 1;
+    }
 public:
     BinaryTree(int root_value) : root(new Node(root_value)) {}
 
@@ -61,6 +70,10 @@ public:
         return get_max(this->root);
     }
 
+    int tree_height() {
+        return get_height(this->root) - 1;
+    }
+
     void print_inorder() {
         print_inorder(root);
         cout << "\n";
@@ -78,7 +91,7 @@ int main() {
     tree.print_inorder();
     // 7 4 8 2 5 9 1 3 10 6
 
-    cout << tree.tree_max();
+    cout << tree.tree_height() << endl;
     return 0;
 
 
