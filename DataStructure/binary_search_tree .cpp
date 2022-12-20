be96dc586d02df33bed5243f9458c8faeca65b4d
HusseinHany <hhany3997@gmail.com>
2022-12-20 11:07:16 +0200

    Insert in BST

--- DataStructure/binary_search_tree .cpp
+++ DataStructure/binary_search_tree .cpp
@@ -19,9 +19,28 @@ public:
 
         return right && right->search(target);
     }
+
+    void insert(int target) {
+        if (target > val) {
+            if (right)
+                right->insert(target);
+            else
+                right = new BST(target);
+        }
+        else if (target < val) {
+            if (left)
+                left->insert(target);
+            else
+                left = new BST(target);
+        }
+    }
 };
 
 int main() {
     BST root(1);
+    root.insert(2);
+    root.insert(3);
+    root.insert(4);
+
     return 0;
 }
