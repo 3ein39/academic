8d4bc8dccdfd8d8b3bdb25f51248ba5feb1b0927
HusseinHany <hhany3997@gmail.com>
2023-02-13 14:29:45 +0200

    Write MinHeap C/D

--- DataStructure/heap.cpp
+++ DataStructure/heap.cpp
@@ -1,3 +1,19 @@
 #include<iostream>
 using namespace std;
 
+class MinHeap {
+private:
+    int* array{};
+    int size {};
+    int capacity {1000}; // as same as capacity in vector ... for simplicity
+public:
+    MinHeap() {
+        array = new int(capacity) {};
+        size = 0;
+    }
+
+    ~MinHeap() {
+        delete[] array;
+        array = nullptr;
+    }
+};
