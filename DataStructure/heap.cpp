a2f71a7e5badd6f3444776871bb5f109b2ba511c
HusseinHany <hhany3997@gmail.com>
2023-02-14 13:38:36 +0200

    Implement pop() -heapify down-

--- DataStructure/heap.cpp
+++ DataStructure/heap.cpp
@@ -33,7 +33,21 @@ private:
         swap(array[child_pos], array[parent_pos]);
         heapify_up(parent_pos);
     }
+    void heapify_down(int parent_pos) {	// O(logn)
+        int child_pos = left(parent_pos);
+        int right_child = right(parent_pos);
 
+        if (child_pos == -1) // no children
+            return;
+        // is right smaller than left?
+        if (right_child != -1 && array[right_child] < array[child_pos])
+            child_pos = right_child;
+
+        if (array[parent_pos] > array[child_pos]) {
+            swap(array[parent_pos], array[child_pos]);
+            heapify_down(child_pos);
+        }
+    }
 public:
     MinHeap() {
         array = new int(capacity) {};
@@ -55,7 +69,11 @@ public:
         assert(!isempty());
         return array[0];
     }
-
+    void pop() {
+        assert(!isempty());
+        array[0] = array[--size];
+        heapify_down(0);
+    }
     ~MinHeap() {
         delete[] array;
         array = nullptr;
