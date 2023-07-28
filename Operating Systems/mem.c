6ba9ab3e5257e15aa91253e2dc2720f19e4e109f
HusseinHany <hhany3997@gmail.com>
2023-07-29 02:34:58 +0300

    Minor change

--- Operating Systems/mem.c
+++ Operating Systems/mem.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include "common.h"
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "usage: mem <value>\n");
@@ -13,7 +15,7 @@ int main(int argc, char *argv[]) {
     printf("(%d) addr pointed to by p: %p\n", (int) getpid(), p);
     *p = atoi(argv[1]); // assign value to addr stored in p
     while (1) {
-        sleep(1);
+        Spin(1);
         *p = *p + 1;
         printf("(%d) value of p: %d\n", getpid(), *p);
     }
